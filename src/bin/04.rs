88bb3615dd71ca73ea64a99fb3ad7a6f51e0e7a7
Allen Gueco <allen.gueco1@gmail.com>
2024-12-09 17:53:00 -0500

    [ci skip]

--- src/bin/04.rs
+++ src/bin/04.rs
@@ -4,6 +4,60 @@ advent_of_code::solution!(4);
 
 const WORD: &str = "XMAS";
 
+#[derive(Debug)]
+struct Puzzle {
+    data: Vec<Vec<char>>,
+    m: usize,
+    n: usize,
+}
+
+impl Index<(usize, usize)> for Puzzle {
+    type Output = char;
+
+    fn index(&self, index: (usize, usize)) -> &Self::Output {
+        &self.data[index.0][index.1]
+    }
+}
+
+impl Puzzle {
+    fn new(input: &str) -> Self {
+        let data: Vec<Vec<char>> = input.lines().map(|l| l.chars().collect()).collect();
+        let m = data.len();
+        let n = data.first().unwrap().len();
+        Self { data, m, n }
+    }
+
+    // fn neighbors(&self, coord: (usize, usize)) -> impl Iterator<Item = Neighbor> {
+    //     Direction::all()
+    //         .map(|d| d.dir())
+    //         .map(move |(i, j)| {
+    //             (
+    //                 i.checked_add_unsigned(coord.0),
+    //                 j.checked_add_unsigned(coord.1),
+    //             )
+    //         })
+    //         .filter_map(move |coord| match coord {
+    //             (Some(i), Some(j)) => Some((i as usize, j as usize)),
+    //             _ => None,
+    //         })
+    //         .filter_map(move |coord| match coord {
+    //             (Some(i), Some(j)) => {
+    //                 if (0..self.m).contains(&i) && (0..self.n).contains(&j) {
+    //                     Some((i, j))
+    //                 } else {
+    //                     None
+    //                 }
+    //             }
+    //             _ => None,
+    //         })
+    //         .filter_map(move |coord| match coord {
+    //             (Some(i), Some(j)) => Some((i, j)),
+    //             _ => None,
+    //         })
+    //         .map(|(i, j)| Neighbor { i, j })
+    // }
+}
+
 pub fn part_one(input: &str) -> Option<u32> {
     let puzzle: Vec<Vec<char>> = input.lines().map(|l| l.chars().collect()).collect();
 
@@ -64,13 +118,22 @@ impl Direction {
 }
 
 fn neighbors(root: (usize, usize)) -> impl Iterator<Item = Neighbor> {
-    Direction::all().map(|d| d.dir()).map(|(i, j)| Neighbor {
-        i: root.0.wrapping_add(i),
-        j: root.1 + j,
-    })
+    Direction::all()
+        .map(|d| d.dir())
+        .map(move |(i, j)| {
+            (
+                i.checked_add_unsigned(root.0),
+                j.checked_add_unsigned(root.1),
+            )
+        })
+        .filter_map(|coord| match coord {
+            (Some(i), Some(j)) => Some((i, j)),
+            _ => None,
+        })
+        .map(|(i, j)| Neighbor { i, j })
 }
 
-fn search(puzzle: &Vec<Vec<char>>, root: (usize, usize)) -> u32 {
+fn search(puzzle: &[Vec<char>], root: (usize, usize)) -> u32 {
     let mut index = 0;
     let mut frontier: Vec<(usize, usize)> = vec![root];
 
@@ -78,6 +141,12 @@ fn search(puzzle: &Vec<Vec<char>>, root: (usize, usize)) -> u32 {
         // If current matches the letter we're looking for
         if WORD.as_bytes()[index] as char == puzzle[current.0][current.1] {
             index += 1;
+        }
+
+        for n in neighbors(current) {
+            if WORD.as_bytes()[index] as char == puzzle[n.i][n.j] {
+                frontier.push((n.i, n.j));
+            }
         }
     }
     0
--- src/bin/05.rs
+++ src/bin/05.rs
@@ -1,5 +1,32 @@
 advent_of_code::solution!(5);
 
+#[derive(Clone, Default)]
+struct Graph {
+    adjacency: Vec<Vec<usize>>,
+}
+
+impl Graph {
+    fn new(input: &str) -> Self {
+        Self {
+            adjacency: Graph::process(input),
+        }
+    }
+
+    fn process(input: &str) -> Vec<Vec<usize>> {
+        let mut list = vec![];
+
+        for r in input.lines() {
+            let p: Vec<usize> = r
+                .split("|")
+                .collect::<Vec<&str>>()
+                .iter()
+                .map(|c| c.parse().unwrap())
+                .collect();
+        }
+
+        list
+    }
+}
 pub fn part_one(input: &str) -> Option<u32> {
     None
 }
